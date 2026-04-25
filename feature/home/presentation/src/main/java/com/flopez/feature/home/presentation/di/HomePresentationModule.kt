c7fd91dc32ff4a7c6181d36295ac452c069c6c53
ferranlopez97 <38299511+ferranlopez97@users.noreply.github.com>
2026-04-25 02:33:01 +0200

    add animations

--- feature/home/presentation/src/main/java/com/flopez/feature/home/presentation/di/HomePresentationModule.kt
+++ feature/home/presentation/src/main/java/com/flopez/feature/home/presentation/di/HomePresentationModule.kt
@@ -5,5 +5,5 @@ import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val homePresentationModule = module {
-    viewModel { HomeViewModel(get()) }
+    viewModel { HomeViewModel(get(), get()) }
 }
